08d07aacdae70436ba534bb6323c37a14c89bf20
Prageeth Silva <prageethsilva@gmail.com>
2020-12-16 16:16:15 +0100

    add 64 bit

--- src/electron/app/index.ts
+++ src/electron/app/index.ts
@@ -2,13 +2,13 @@ import { ipcMain } from 'electron';
 
 // TESTING
 // =================================
-ipcMain.on('asynchronous-message', (event, arg) => {
+ipcMain.on('asynchronous-message', (event: any, arg: any) => {
   // prints "ping"
   console.log(arg); // tslint:disable-line no-console
   event.reply('asynchronous-reply', 'pong');
 });
 
-ipcMain.on('synchronous-message', (event, arg) => {
+ipcMain.on('synchronous-message', (event: any, arg: any) => {
   // prints "ping"
   console.log(arg); // tslint:disable-line no-console
   event.returnValue = 'pong';
