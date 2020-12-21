48e1da326fbefc05d78353667f573982bb1b1680
Prageeth Silva <prageethsilva@gmail.com>
2020-12-21 22:58:04 +0100

    remove exiting archives

--- scripts/build-archives.sh
+++ scripts/build-archives.sh
@@ -4,6 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR/../dist
 
+echo "Removing exiting archives..."
+rm -fv *.tar.gz
+
 for f in ./*; do
   if [ -d "$f" ]; then
     echo "Archiving Starting... $f"
